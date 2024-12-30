9bae03674dc16d82e4167dcf2013d79ae2a5d8da
Gareth Coles <gdude2002@gmail.com>
2024-12-30 09:55:42 +0000

    i18n: Support class-loader callbacks

--- i18n/src/main/kotlin/dev/kordex/i18n/Translations.kt
+++ i18n/src/main/kotlin/dev/kordex/i18n/Translations.kt
@@ -6,6 +6,7 @@
 
 package dev.kordex.i18n
 
+import dev.kordex.i18n.registries.ClassLoaderRegistry
 import io.github.oshai.kotlinlogging.KLogger
 import io.github.oshai.kotlinlogging.KotlinLogging
 import java.util.*
@@ -136,13 +137,57 @@ public object Translations {
 		}
 	}
 
-	private fun getResourceBundle(bundle: Bundle, locale: Locale): ResourceBundle =
-		ResourceBundle.getBundle(
+	private fun getResourceBundle(bundle: Bundle, locale: Locale): ResourceBundle {
+		val baseName = bundle.name.replace(".", "/")
+		val control = bundle.getResourceBundleControl()
+		val format = control.getFormats(baseName)
+			.map { it.split(".").last() }
+			.first { it != "class" }
+
+		val bundlePath = "$baseName.$format"
+
+		val classLoaders: MutableList<Pair<String, ClassLoader>> = mutableListOf(
+			"bundle" to bundle.classLoader,
+			"current" to Translations::class.java.classLoader,
+			"system" to ClassLoader.getSystemClassLoader(),
+		)
+
+		classLoaders.addAll(
+			ClassLoaderRegistry.getCallbackClassloaders().entries.map { it.toPair() }
+		)
+
+		logger.trace {
+			"Searching for $bundle with locale ${locale.toLanguageTag()} in ${classLoaders.size} class-loaders"
+		}
+
+		for ((name, loader) in classLoaders) {
+			logger.trace { "Searching in class-loader $name" }
+
+			try {
+				if (loader.getResource(bundlePath) != null) {
+					val result = ResourceBundle.getBundle(bundle.name, locale, loader, control)
+
+					logger.debug { "Found $bundle with locale ${locale.toLanguageTag()} in $name class-loader" }
+
+					return result
+				}
+			} catch (_: MissingResourceException) {
+				// Do nothing, move on to the next loader.
+				continue
+			}
+		}
+
+		logger.debug {
+			"Couldn't find $bundle with locale ${locale.toLanguageTag()} in ${classLoaders.size} class-loaders; " +
+				"falling back to default strategy"
+		}
+
+		return ResourceBundle.getBundle(
 			bundle.name,
 			locale,
-			bundle.classLoader,
 			bundle.getResourceBundleControl()
 		)
+	}
 
 	@Suppress("DEPRECATION")  // Locale constructor, for compatibility with Java versions older than 19.
 	private fun getBundles(bundle: Bundle, locale: Locale): Pair<ResourceBundle, ResourceBundle?> {
--- i18n/src/main/kotlin/dev/kordex/i18n/registries/ClassLoaderRegistry.kt
+++ i18n/src/main/kotlin/dev/kordex/i18n/registries/ClassLoaderRegistry.kt
@@ -10,15 +10,34 @@ import dev.kordex.i18n.Bundle
 import io.github.oshai.kotlinlogging.KLogger
 import io.github.oshai.kotlinlogging.KotlinLogging
 
+public typealias ClassLoaderCallback = () -> Map<String, ClassLoader>
+
 public object ClassLoaderRegistry {
-	private val logger: KLogger = KotlinLogging.logger { }
+	private val logger: KLogger = KotlinLogging.logger("dev.kordex.i18n.registries.ClassLoaderRegistry")
+
 	private val bundleCache = mutableMapOf<String, ClassLoader>()
+	private val callbacks: MutableSet<ClassLoaderCallback> = mutableSetOf()
 
 	public fun getForBundle(bundle: String): ClassLoader =
 		bundleCache[bundle] ?: ClassLoader.getSystemClassLoader()
 
 	public fun getForBundle(bundle: Bundle): ClassLoader =
 		getForBundle(bundle.name)
+
+	@Suppress("TooGenericExceptionCaught")
+	public fun getCallbackClassloaders(): Map<String, ClassLoader> =
+		callbacks
+			.map {
+				try {
+					it.invoke()
+				} catch (e: Exception) {
+					logger.warn(e) { "Exception thrown by classloader callback $it - skipping..." }
+
+					mapOf()
+				}
+			}
+			.flatMap { it.entries }
+			.associate(Map.Entry<String, ClassLoader>::toPair)
 
 	public fun register(bundle: String, classLoader: ClassLoader): Boolean {
 		if (bundle !in bundleCache) {
@@ -34,4 +53,16 @@ public object ClassLoaderRegistry {
 
 	public fun register(bundle: Bundle): Boolean =
 		register(bundle.name, bundle.classLoader)
+
+	public fun register(callback: ClassLoaderCallback): Boolean {
+		val result = callbacks.add(callback)
+
+		if (result) {
+			logger.trace { "Registered class-loader callback: $callback" }
+		} else {
+			logger.trace { "Class-loader callback already registered: $callback" }
+		}
+
+		return result
+	}
 }
